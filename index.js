71aef8d1ba0375b8c27e346c89c58fd4bb54c45b
Hitesh Kumar <hiteshkumar0778@gmail.com>
2025-03-07 22:31:44 +0530

    Checking If Working

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("some content inside index.js");
+// kyu aap apni gand nhi marva lete
+console.log("modified content inside index.js")
+
+console.log("changed added in the feature branch")
+
+console.log("first");
+console.log("second");
+
+console.log("first in feat");
+
+console.log("sec in feat")
+
+let f = 5;
+console.log(5);
+
+let s = 10;
+console.log(s);
+
+
+let sh = 10;
+console.log(sh);
