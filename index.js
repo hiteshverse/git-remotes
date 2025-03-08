c46c13fc9f9758beb71f6fc6ab6664ad6c1817be
Hitesh Kumar <hiteshkumar0778@gmail.com>
2025-03-08 11:11:30 +0530

    Revert "second"
    This reverts commit a1fa56b014b99f213796a9eaed4e359e660fde9e.

--- index.js
+++ index.js
@@ -3,5 +3,3 @@ console.log("some content inside index.js");
 console.log("modified content inside index.js")
 
 console.log("changed added in the feature branch")
-
-console.log("changed in the feature branhc sec");
